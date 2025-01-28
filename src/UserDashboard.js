dc31f2e63ba34bc963eb303c1bacab517388447a
kunaljani1100 <168258319+kunaljani1100@users.noreply.github.com>
2025-01-27 23:55:25 -0700

    Get user dashboard after authentication.

--- src/UserDashboard.js
+++ src/UserDashboard.js
@@ -1,6 +1,44 @@
+import ReactDOM from 'react-dom/client';
+
 function UserDashboard(props) {
-    return (
-        <h1>Welcome {props.username}</h1>
+    const request = {
+        username: props.username
+    }
+    const message = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(request)
+    }
+    fetch("http://localhost:8080/users/get", message)
+    .then(response => {
+        return response.json()
+    })
+    .then(data => {
+        const root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(
+            <div>
+                <h1>Gri</h1>
+                <UserInformation firstName = {data.firstName} lastName = {data.lastName} middleName = {data.middleName} groups = {data.groups}/>
+            </div>
+        )
+    })
+}
+
+function UserInformation(props) {
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    console.log(props.groups)
+    root.render(
+        <div>
+            <h1>{props.firstName} {props.middleName} {props.lastName}</h1>
+            <p>Groups you are a member of:</p>
+            <p>
+            {props.groups.map((group, index) => ( 
+                <p><button value = {group}>{group.split("::")[0]}</button></p>
+            ))}
+            </p>
+        </div>
     )
 }
 
